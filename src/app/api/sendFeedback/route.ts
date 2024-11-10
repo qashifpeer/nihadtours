788a021a6553d26d93757bcc4d1b079e258094a8
qashifpeer <mail.qashifpeer@gmail.com>
2024-11-10 12:14:41 +0530

    host exposed for trial

--- src/app/api/sendFeedback/route.ts
+++ src/app/api/sendFeedback/route.ts
@@ -7,7 +7,8 @@ export async function POST(request: Request) {
   const { name, phone, arrivalDate } = await request.json();
 
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    // host: process.env.EMAIL_HOST,
+     host: 'smtp.hostinger.com',
     port: parseInt(process.env.EMAIL_PORT || '587'),
     secure: true,
     auth: {
